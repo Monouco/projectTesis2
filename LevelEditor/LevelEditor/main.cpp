393643ccc308757361af9ee2c85384d11f095064
Monouco <algoalgo175@gmail.com>
2022-07-20 12:03:27 -0500

    Colored window

--- LevelEditor/LevelEditor/main.cpp
+++ LevelEditor/LevelEditor/main.cpp
@@ -12,6 +12,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 800
+
 using namespace std;
 
 /*
@@ -19,24 +22,44 @@ using namespace std;
  */
 int main(int argc, char** argv) {
 
+    //Init GLFW
     glfwInit();
 
+    //Specify which version of OpenGL to use (3.3)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //Specify which profile to use (CORE = modern functions)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Test", NULL, NULL);
+    //Create a window
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sexo", NULL, NULL);
+    //Error handling
     if (window == NULL) {
         cout << "Failed to create GLFW window" << endl;
         glfwTerminate();
         return -1;
     }
+    //add window to context
     glfwMakeContextCurrent(window);
 
+    //Load GLAD to configure OpenGL
+    gladLoadGL();
+
+    //Specify the viewport, the window is from 0 to 800
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    //Specify the color
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+
+    //Loop where user interacts with the window
     while (!glfwWindowShouldClose(window)) {
+        //Get events on window
         glfwPollEvents();
     }
 
+    //Destroy and end GLFW
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
